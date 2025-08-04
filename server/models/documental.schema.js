4187af3a2899870c5ee6eb6ea98ee4e6e7298313
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2025-08-04 13:25:49 -0600

    documental schema individual description

--- server/models/documental.schema.js
+++ server/models/documental.schema.js
@@ -7,64 +7,64 @@ export const Documental = defineMongooseModel({
 
     schema: {
         identificacion: {
-            codigoReferencia: {type: 'string', required: true, trim: true, unique: true },
-            referenciaProcedencia: {type: 'string', trim: true},
-            titulo: {type: 'string', trim: true},
+            codigoReferencia: {type: 'string', required: true, trim: true }, // 1 palabra (ej. MXIM-AV-21-1-7)
+            referenciaProcedencia: {type: 'string', trim: true}, // nombre propio
+            titulo: {type: 'string', trim: true}, // nombre propio
             fecha: {type: 'string', trim: true},
-            tituloSerie: {type: 'string', trim: true},
-            numeroPrograma: {type: 'string', trim: true},
-            pais: {type: 'string', trim: true},
+            tituloSerie: {type: 'string', trim: true}, // nombre propio
+            numeroPrograma: {type: 'string', trim: true}, // nombre propio
+            pais: {type: 'string', trim: true}, // nombre propio
             duracion: {type: 'number'}, // total de minutos
-            realizacion: {type: 'string', trim: true},
-            investigacion: {type: 'string', trim: true},
-            ideaOriginal: {type: 'string', trim: true},
-            guion: {type: 'string', trim: true},
-            camara: {type: 'string', trim: true},
-            sonido: {type: 'string', trim: true},
-            fotografia: {type: 'string', trim: true},
-            animacion: {type: 'string', trim: true},
-            musicaOriginal: {type: 'string', trim: true},
-            musicalizacion: {type: 'string', trim: true},
-            direccionActuacion: {type: 'string', trim: true},
-            direccion: {type: 'string', trim: true},
-            voces: {type: 'string', trim: true},
-            otrosColaboracion: {type: 'string', trim: true},
+            realizacion: {type: 'string', trim: true}, // nombres de personas
+            investigacion: {type: 'string', trim: true}, // nombres de personas
+            ideaOriginal: {type: 'string', trim: true}, // nombres de personas
+            guion: {type: 'string', trim: true}, // nombres de personas
+            camara: {type: 'string', trim: true}, // nombres de personas
+            sonido: {type: 'string', trim: true}, // nombres de personas
+            fotografia: {type: 'string', trim: true}, // nombres de personas
+            animacion: {type: 'string', trim: true}, // nombres de personas
+            musicaOriginal: {type: 'string', trim: true}, // nombres de personas
+            musicalizacion: {type: 'string', trim: true}, // nombres de personas
+            direccionActuacion: {type: 'string', trim: true}, // nombres de personas
+            direccion: {type: 'string', trim: true}, // nombres de personas
+            voces: {type: 'string', trim: true}, // nombres de personas
+            otrosColaboracion: {type: 'string', trim: true}, // nombres de personas
         },
         contexto: {
-            entidadProductora: {type: 'string', trim: true},
-            produccion: {type: 'string', trim: true},
-            distribuidora: {type: 'string', trim: true},
-            historiaInstitucional: {type: 'string', trim: true},
-            resenaBiografica: {type: 'string', trim: true},
+            entidadProductora: {type: 'string', trim: true}, // nombre propio
+            produccion: {type: 'string', trim: true}, // nombre propio
+            distribuidora: {type: 'string', trim: true}, // nombre propio
+            historiaInstitucional: {type: 'string', trim: true}, // sinopsis (1 o más párrafos)
+            resenaBiografica: {type: 'string', trim: true}, // sinopsis (1 o más párrafos)
         },
         contenidoEstructura: {
-            sinopsis: {type: 'string', trim: true},
-            descriptorOnomastico: {type: 'string', trim: true},
-            descriptorToponimico: {type: 'string', trim: true},
-            descriptorTopografico: {type: 'string', trim: true},
+            sinopsis: {type: 'string', trim: true}, // 1 o más párrafos
+            descriptorOnomastico: {type: 'string', trim: true}, // lista de nombres propios
+            descriptorToponimico: {type: 'string', trim: true}, // lista de fechas
+            descriptorTopografico: {type: 'string', trim: true},  // lista de lugares
             fuentes: {type: 'string', trim: true}, // TO DO: Definir lista
             recursos: {type: 'string', trim: true}, // TO DO: Definir lista
         },
         condicionesAccesoUso: {
-            idiomaOriginal: {type: 'string', trim: true},
+            idiomaOriginal: {type: 'string', trim: true}, // default: Español
             subtitulaje: {type: 'string', trim: true},
             color: {type: 'string', enum: ['Color', 'Blanco y negro']},
             soporteOriginal: {type: 'string', enum: ['Betacam', 'Hi8', 'DVCAM', 'MiniDV', 'VHS']},
         },
         documentacionAsociada: {
-            originales: {type: 'string', trim: true},
-            unidadesRelacionadas: {type: 'string', trim: true},
-            notasPublicacion: {type: 'string', trim: true},
+            originales: {type: 'string', trim: true}, // extensión aproximada de 1 párrafo
+            unidadesRelacionadas: {type: 'string', trim: true}, // extensión aproximada de 1 párrafo
+            notasPublicacion: {type: 'string', trim: true}, // extensión aproximada de 1 párrafo
         },
         notas: {
-            notas: {type: 'string', trim: true},
+            notas: {type: 'string', trim: true}, // extensión máxima de 3 párrafo
         },
         controlDescripcion: {
-            nombreCatalogacion: {type: 'string', trim: true},
+            nombreCatalogacion: {type: 'string', trim: true}, // nombre propio de una persona
         },
         adicional: {
-            imagen: {type: 'string', trim: true}, // referencia a la portada
-            clipVideo: {type: 'string', trim: true}, // referencia al clip de video
+            imagen: {type: 'string', trim: true}, // referencia a la portada (ej.: "documental_1.jpg")
+            clipVideo: {type: 'string', trim: true}, // referencia al clip de video (ej.: "documental_1.mp4")
         },
     },
     
