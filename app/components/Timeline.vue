e5c245500d234f727023af54973990195e3eca65
Checod <sergioamarocs@gmail.com>
2025-08-18 17:48:28 -0600

    Se agrega responsive de la line adel tiempo

--- app/components/Timeline.vue
+++ app/components/Timeline.vue
@@ -1,21 +1,7 @@
 <template>
     <div class="touch-pan-x -webkit-overflow-scrolling-touch timeline-wrapper overflow-x-hidden hover:overflow-x-scroll onTouchMove:overflow-x-scroll">
         <div class="flex flex-nowrap">
-            <!-- <div v-for="(item, index) in items" :key="index" class="mr-4 relative">
-                <UTimeline orientation="horizontal" :default-value="1" :items="[item]" class="w-full">
-                    <template #item="{ item }">
-                        <div class="flex flex-row">
-                            <img :src="item.image" alt="Image" class="w-20 h-20 mr-4" />
-                            
-                            <div>
-                                <h2>{{ item.title }}</h2>
-                                <p>{{ item.description }}</p>
-                            </div>
-                        </div>
-                    </template>
-                </UTimeline>
-            </div> -->
-            <UTimeline orientation="horizontal" :items="items" class="w-full" />
+            <UTimeline :orientation="isMobile ? 'vertical' : 'horizontal'" :items="items" class="w-full" color="neutral" />
         </div>
     </div>
 </template>
@@ -41,4 +27,15 @@ const newItems = props.data.map((documentalista) => {
 })
 // agregar reactividad a nueva lista de objetos para timeline
 const items = ref(newItems)
+
+
+// Comprobación de dispositivos móviles
+let isMobile = ref(false);
+if(window !== undefined){
+    isMobile.value = window.innerWidth < 768
+    window.addEventListener('resize', () => {
+        isMobile.value = window.innerWidth < 768
+    })
+}
+
 </script>
