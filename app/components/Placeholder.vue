bf193593ad1995efa1aed91412383877e1ac9cbd
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2025-08-21 17:45:06 -0600

    new component Placeholder added

---
+++ app/components/Placeholder.vue
@@ -0,0 +1,13 @@
+<!-- Componente para mostrar un espacio sin contenido definido -->
+<template>
+    <!-- Cuadrícula de puntos con borde punteado -->
+    <div class="m-2 rounded-lg outline outline-neutral-800 outline-dashed outline-offset-4" :style="{'height': height + 'px'}">
+        <div class="h-full inset-0 w-full bg-transparent bg-[radial-gradient(#333_1px,transparent_1px)] [background-size:16px_16px]"></div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    height: { type: Number, default: 128 } // altura
+})
+</script>
