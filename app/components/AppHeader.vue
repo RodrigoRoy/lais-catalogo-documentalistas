c595bc4904857608d411b35a9b683f5c371ef3a3
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2025-09-03 16:38:52 -0600

    updated navbar

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,5 +1,9 @@
 <template>
-  <UNavigationMenu :items="items" color="primary" :highlight="false" highlight-color="primary" variant="link" content-orientation="vertical" class="justify-center h-16 bg-primary/10" />
+  <div class="flex justify-evenly bg-primary/20 h-16">
+    <img src="/Mujeres_documentalistas_logo.png">
+    <UNavigationMenu :items="items" color="primary" :highlight="false" highlight-color="primary" variant="link" content-orientation="vertical" />
+    <UInput color="primary" variant="subtle" icon="i-lucide-search" size="md" placeholder="Buscar..." />
+  </div>
 </template>
 
 <script setup>
@@ -15,41 +19,48 @@ const items = ref([
     to: '/documentalistas',
     children: [
       {
-        label: 'Rosa Martha Fernández',
-        icon: 'i-lucide-smile',
-        to: '/rosa-martha'
-      },
-      {
-        label: 'Alejandra Islas',
-        icon: 'i-lucide-smile',
-        to: '/alejandra-islas'
-      },
-      {
-        label: 'Bertha Navarro',
-        icon: 'i-lucide-smile',
-        to: '/bertha-navarro'
-      },
-      {
         label: 'Guadalupe Sánchez Sosa',
         icon: 'i-lucide-smile',
         to: '/lupita-sanchez'
       },
-      {
-        label: 'Maru Tamés',
-        icon: 'i-lucide-smile',
-        to: '/maru-tames'
-      },
-      {
-        label: 'Olivia Carrión',
-        icon: 'i-lucide-smile',
-        to: '/olivia-carrion'
-      },
+      // {
+      //   label: 'Rosa Martha Fernández',
+      //   icon: 'i-lucide-smile',
+      //   to: '/rosa-martha'
+      // },
+      // {
+      //   label: 'Alejandra Islas',
+      //   icon: 'i-lucide-smile',
+      //   to: '/alejandra-islas'
+      // },
+      // {
+      //   label: 'Bertha Navarro',
+      //   icon: 'i-lucide-smile',
+      //   to: '/bertha-navarro'
+      // },
+      // {
+      //   label: 'Maru Tamés',
+      //   icon: 'i-lucide-smile',
+      //   to: '/maru-tames'
+      // },
+      // {
+      //   label: 'Olivia Carrión',
+      //   icon: 'i-lucide-smile',
+      //   to: '/olivia-carrion'
+      // },
     ]
   },
   {
     label: 'Filmografía',
     icon: 'i-bx-camera-movie',
-    to: '/documentales/filmografia'
+    to: '/documentales/filmografia',
+    children: [
+      {
+        label: 'Xantolo',
+        icon: 'i-mdi-movie',
+        to: '/documentales/xantolo-fiesta-de-todos-los-santos'
+      },
+    ]
   },
   {
     label: 'Línea de tiempo',
