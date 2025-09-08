d2c0c6a4ac9be8823a7e7d908f9bf2e63118d2bf
Checod <sergioamarocs@gmail.com>
2025-09-08 13:23:39 -0600

    Se agregan comentarios

--- app/components/CloudWords.vue
+++ app/components/CloudWords.vue
@@ -18,49 +18,46 @@
                 "Documentales",
             ];
 
-            var fontFamily = "sans-serif";
-            var fontScale = 25;
-            var padding = 0;
-            var height = 500;
-            var width = 700;
+            const fontFamily = "sans-serif";
+            const fontScale = 25;
+            const padding = 0;
+            const height = 500;
+            const width = 700;
             const rotate = () => 0; // () => (~~(Math.random() * 6) - 3) * 30
 
-            var data = d3
-                .rollups(
+            var data = d3.rollups(
                 words,
                 (group) => group.length,
                 (w) => w
-                )
-                .sort(([, a], [, b]) => d3.descending(a, b))
-                .slice(0, 250)
-                .map(([text, value]) => ({ text, value }));
-            console.log(data);
+            )
+            .sort(([, a], [, b]) => d3.descending(a, b))
+            .slice(0, 250)
+            .map(([text, value]) => ({ text, value }));
 
             const svg = d3
                 .select(this.$refs.chartContainer)
                 .append("svg")
-                .attr("height", height)
-                .attr("width", width)
-                .attr("font-family", fontFamily)
-                .attr("text-anchor", "middle")
-                .attr("fill", "white");
+                .attr("height", height) // Altura del texto
+                .attr("width", width) // Ancho del texto
+                .attr("font-family", fontFamily) // Fuente del texto
+                .attr("text-anchor", "middle") // Alinear el texto
+                .attr("fill", "white") // Cambiar color de las palabras
+                .on("click", (d) => console.log("Redireccion: ", d.srcElement.innerHTML)); // Agregar funcion de click
 
             const w_cloud = cloud()
-                .size([width, height])
-                .words(data.map((d) => Object.create(d)))
-                .padding(padding)
-                .rotate(rotate)
-                .font(fontFamily)
-                .fontSize((d) => Math.sqrt(d.value) * fontScale)
-                .on("word", ({ size, x, y, rotate, text }) => {
-                svg
-                    .append("text")
-                    .attr("font-size", size)
-                    .attr("transform", `translate(${x},${y}) rotate(${rotate})`)
-                    .attr("color", "white")
-                    .text(text);
-                });
-
+            .size([width, height])
+            .words(data.map((d) => Object.create(d)))
+            .padding(padding)
+            .rotate(rotate)
+            .font(fontFamily)
+            .fontSize((d) => Math.sqrt(d.value) * fontScale)
+            .on("word", ({ size, x, y, rotate, text }) => {
+            svg
+                .append("text")
+                .attr("font-size", size) // Tamaño de la palabra
+                .attr("transform", `translate(${x},${y}) rotate(${rotate})`) // Posicion de la palabra
+                .text(text); // Texto
+            })
             w_cloud.start();
         }
     }
