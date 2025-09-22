be2370b49c5c95c0ff34e5d23e84e109745a7992
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2025-09-22 14:52:55 -0600

    theme colors

---
+++ app/app.config.js
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      // Quizá se requieran definir colores con mayor precisión
+      // Revisar documentación en caso de ser necesario: https://ui.nuxt.com/getting-started/theme#tailwind-css
+      primary: 'sky',
+      secondary: 'red',
+      neutral: 'zinc',
+    }
+  }
+})
