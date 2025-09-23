a84be313c3609205c4e9a74f1b4a330075dafe71
Checod <sergioamarocs@gmail.com>
2025-09-23 13:29:07 -0600

    Primera version de linea del tiempo

--- app/pages/linea-de-tiempo.vue
+++ app/pages/linea-de-tiempo.vue
@@ -2,16 +2,18 @@
     <AppTitle title="Linea del tiempo" subtitle="Página con los documentales y eventos más relevantes" />
     <p class="text-sm font-normal text-justify">{{ loremIpsum }}</p>
     <Timeline :data="documentalistas" class="mt-8" />
+    <Timeline3d id="timeline1"/>
+    <Timeline3d id="timeline2"/>
 </template>
 
 <script setup>
-definePageMeta({
-    title: 'Línea del tiempo - Mujeres Documentalistas en México',
-})
+    definePageMeta({
+        title: 'Línea del tiempo - Mujeres Documentalistas en México',
+    })
 
-// Información de documentalistas
-const { data:documentalistas } = await useFetch('/api/documentalistas', {query: {sortBy: 'fechaNacimiento'}})
+    // Información de documentalistas
+    const { data:documentalistas } = await useFetch('/api/documentalistas', {query: {sortBy: 'fechaNacimiento'}})
 
-// Texto de prueba
-const loremIpsum = ref('Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\n\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\n\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.')
+    // Texto de prueba
+    const loremIpsum = ref('Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\n\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\n\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.')
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://unpkg.com/d3-milestones/build/d3-milestones.css', // External stylesheet URL
+        }
+      ],
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/image', '@nuxt/ui', 'nuxt-mongoose', 'nuxt-mongoose'],
