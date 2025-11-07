4e30281d8bf50d7c920589b4a8d60624acc7e069
Rodrigo Colín <rodrigo.colin.rivera@gmail.com>
2025-11-07 03:36:19 -0600

    error component added

---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <UApp>
+    <AppHeader />
+
+    <UError :error="error" />
+
+    <AppFooter />
+  </UApp>
+</template>
+
+<script setup>
+const props = defineProps('error')
+</script>
